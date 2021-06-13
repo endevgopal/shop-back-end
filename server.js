4728a487d9bbc6ba060e4d930709280e323234b1
endevgopal <gopal.ukani1@gmail.com>
2021-06-14 00:44:13 +0530

    change in productionr

--- server.js
+++ server.js
@@ -41,11 +41,11 @@ app.use('/api', upRouter);
 app.use('/api', productRouter);
 app.use('/api', paymentRouter);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static('client/build'))
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,'client',"build","index.html"))
-  })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
