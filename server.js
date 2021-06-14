83ed1ddde29f4922b621e7501c24cda0d1f99c6a
endevgopal <gopal.ukani1@gmail.com>
2021-06-14 18:20:09 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.use(
     useTempFiles: true,
   })
 );
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});  
 
 const url = process.env.MONGO_URL;
 mongoose.connect(
