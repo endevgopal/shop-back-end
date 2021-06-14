f73153b40ee2e103d3231d92b24c7d1951b95fd7
endevgopal <gopal.ukani1@gmail.com>
2021-06-14 11:55:22 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/api', productRouter);
 app.use('/api', paymentRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
@@ -57,3 +57,4 @@ app.listen(process.env.PORT || 5000, function () {
     app.settings.env
   );
 });
+
